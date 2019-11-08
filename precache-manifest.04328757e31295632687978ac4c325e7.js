fc815a49f52b946b8b64a32f43f406913c8777b7
Dmitriymush <powder322@gmail.com>
2019-11-08 03:08:53 +0200

    Updates

---
+++ precache-manifest.04328757e31295632687978ac4c325e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de3464d2e4a6a7771f2f73b8b752fd57",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "ac2b11588a346a2ee7bd",
+    "url": "/react_tabs-with-router/static/css/main.9a6a139a.chunk.css"
+  },
+  {
+    "revision": "502945b425e3b3f604fb",
+    "url": "/react_tabs-with-router/static/js/2.716802d7.chunk.js"
+  },
+  {
+    "revision": "ac2b11588a346a2ee7bd",
+    "url": "/react_tabs-with-router/static/js/main.a58ac2c3.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
